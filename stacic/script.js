c1f130bb08a0b048ef24c1f42cd5c89296ce4cab
rafafeltrin <rafaelfl.rocha.feltrin@gmail.com>
2022-10-01 22:07:34 -0300

    contato

--- stacic/script.js
+++ stacic/script.js
@@ -42,3 +42,24 @@ function typeWriter(elemento) {
 
 const titulo2 = document.querySelector('span');
 typeWriter(titulo2);
+
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
+
+  item.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", ()=>{
+  animeScroll();
+})
